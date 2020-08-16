1f58e3f44b00e2485efc87bb49bd2e9cc4c965bc
bss1157 <50788267+bssjgb1157@users.noreply.github.com>
2020-08-16 23:22:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,14 +48,14 @@ download()
 		      more /$iam/.pip/pip.conf
 	fail
 	if [ -e /usr/bin/wget -a /usr/bin/unzip ];then
-		wget https://github.com/janeczku/calibre-web/archive/master.zip
-	        unzip master.zip
+		wget https://github.com/janeczku/calibre-web/archive/master.zip -O cali.zip
+	        unzip cali.zip
 		cont
 	else
 		apt install wget -y
 		apt install unzip -y
-		wget https://github.com/janeczku/calibre-web/archive/master.zip
-	unzip master.zip
+		wget https://github.com/janeczku/calibre-web/archive/master.zip -O cali.zip
+	unzip cali.zip
 cont	
 	fi
 		}
